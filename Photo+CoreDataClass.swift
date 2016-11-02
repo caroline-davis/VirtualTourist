bfb9c4cd0b66bd481e593d8dfacc6d0534c41f40
Caroline Davis <caroline_davis@live.com>
2016-11-02 12:00:31 +1100

    feat: Add inits to model class pin and photo

--- Photo+CoreDataClass.swift
+++ Photo+CoreDataClass.swift
@@ -11,5 +11,19 @@ import CoreData
 
 
 public class Photo: NSManagedObject {
+    
+    // MARK: Initializer
+    convenience init(image: NSData, context: NSManagedObjectContext){
+        
+        // An EntityDescription is an object that has access to all
+        // the information you provided in the Entity part of the model
+        // you need it to create an instance of this class.
+        if let ent = NSEntityDescription.entity(forEntityName: "Photo", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.image = image
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
 
 }
--- Pin+CoreDataClass.swift
+++ Pin+CoreDataClass.swift
@@ -11,5 +11,20 @@ import CoreData
 
 
 public class Pin: NSManagedObject {
+    
+    // MARK: Initializer
+    convenience init(latitude: Double = 0.0, longitude: Double = 0.0, context: NSManagedObjectContext) {
+        
+        // An EntityDescription is an object that has access to all
+        // the information you provided in the Entity part of the model
+        // you need it to create an instance of this class.
+        if let ent = NSEntityDescription.entity(forEntityName: "Pin", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.latitude = latitude
+            self.longitude = longitude
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
 
 }
